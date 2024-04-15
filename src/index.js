82f64595a537268eb920a3edeedc0632c9c66980
jagudeloq <jagudeloq@COR-DLLOSOFT34>
2024-04-15 09:20:45 -0500

    update message

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+    console.log(`Servidor corriendo en https://first-server-dev-njrm.3.us-1.fl0.io/ puerto:${port}`);
 });
